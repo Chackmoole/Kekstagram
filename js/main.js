3b3aac9c57fbd886f56cd460c23e71037b09fc53
Chackmoole <cebep-21@mail.ru>
2025-06-02 18:01:54 +0300

    Добавлены данные. Уточнены параметры данных.

--- js/main.js
+++ js/main.js
@@ -21,10 +21,20 @@ const checkStringLength = (str, maxLength) => typeof maxLength === 'number' && t
 
 checkStringLength('   qwertqwert  ', 10);
 
+const PHOTO_COUNT = 25;
 const MIN_ID = 1;
-const MAX_ID = 25;
+const MAX_ID = PHOTO_COUNT;
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
 
-const createIdArray = (minId, maxId) => {
+const comments = ['Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+
+const createDataArray = (minId, maxId) => {
   const tempArray = [];
   for (let i = minId; i <= maxId; i++) {
     tempArray.push(i);
@@ -32,14 +42,15 @@ const createIdArray = (minId, maxId) => {
   return tempArray;
 };
 
-createIdArray(MIN_ID, MAX_ID);
+createDataArray(MIN_ID, MAX_ID);
 
 const createPhotoDescription = () => ({
   id: 1,
-  url: '',
-  description: '',
-  likes: 2,
-  comments: [1, 2, 3]
+  url: `photos/${MIN_ID}.jpg`,
+  description: comments[0],
+  likes: MAX_LIKES - MIN_LIKES,
+  comments: [{ MIN_ID: comments[0] }],
+  avatar: `img/avatar-${MIN_ID}.svg`
 });
 
 createPhotoDescription();
