103ae9cec59b2f6870a8a1e6304fb9ab3f5a0e6c
Chackmoole <cebep-21@mail.ru>
2025-06-09 12:20:39 +0300

    Вынесена логика вспомогателных функций в файл util.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,4 @@
-const getRandomNumber = (min, max) => {
-  if (typeof min !== 'number' || typeof max !== 'number' || min < 0 || max < 0) {
-    return null;
-  }
-
-  //если значения равны
-  if (min === max) {
-    return min;
-  }
-  //получаем минимальное и максимальное значение
-  const minNumber = Math.min(min, max);
-  const maxNumber = Math.max(min, max);
-  // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-  return Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
-};
-
-getRandomNumber(0, 1);
-
-
-const checkStringLength = (str, maxLength) => typeof maxLength === 'number' && typeof str === 'string' && str.trim().length <= maxLength;
-
-checkStringLength('   qwertqwert  ', 10);
+import { getRandomNumber } from './util.js';
 
 const PHOTO_COUNT = 25;
 const MIN_ID = 1;
@@ -119,7 +98,6 @@ const createPhotosDesc = () => {
   const tempArray = Array.from({ length: PHOTO_COUNT }, createPhotoDescription);
   return tempArray;
 };
-
 
 createPhotosDesc();
 
