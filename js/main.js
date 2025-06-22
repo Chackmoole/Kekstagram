e8ec708852477c39b48e1fd0f31b9d04587ac600
Chackmoole <cebep-21@mail.ru>
2025-06-22 12:04:52 +0300

    Добавлен src для большой фотографии

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { fragment } from './renderPhotos.js';
+import { photosList } from './renderBigPhoto.js';
 
 const photoList = document.querySelector('.pictures');
 
 photoList.appendChild(fragment);
-
+console.log(photosList);
--- js/renderBigPhoto.js
+++ js/renderBigPhoto.js
@@ -5,7 +5,11 @@ const bigPicture = document.querySelector('.big-picture');
 const addBigPhotoHadler = (item) => {
   item.addEventListener('click', (evt) => {
     evt.preventDefault();
+
     bigPicture.classList.remove('hidden');
+
+    bigPicture.querySelector('img').src = item.querySelector('img').src;
+
 
   });
 };
