b01725314f89d55beec5dfa832f1e11e88c53671
Chackmoole <cebep-21@mail.ru>
2025-06-26 16:52:55 +0300

    Настройка параметров формы

--- js/renderPreview.js
+++ js/renderPreview.js
@@ -1,1 +1,11 @@
-export const renderPreview = () => { console.log('1'); };
+// const fileUploadElement = document.querySelector('#upload-file');
+const previewPhoto = document.querySelector('.img-upload__overlay');
+const elementBody = document.querySelector('body');
+
+export const renderPreview = () => {
+  previewPhoto.classList.remove('hidden');
+  elementBody.classList.remove('modal-open');
+  // console.log(fileUploadElement);
+
+
+};
