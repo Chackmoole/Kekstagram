27f428a8a34b9e018a7091f43b35844f48bd3c4b
Chackmoole <cebep-21@mail.ru>
2025-06-27 17:47:42 +0300

    Добавлено открытие и закрытие окна редактирования.

--- js/renderPreview.js
+++ js/renderPreview.js
@@ -1,11 +1,33 @@
-// const fileUploadElement = document.querySelector('#upload-file');
 const previewPhoto = document.querySelector('.img-upload__overlay');
 const elementBody = document.querySelector('body');
+const uploadCancel = document.querySelector('#upload-cancel');
+
+
+const closeModal = () => {
+  previewPhoto.classList.add('hidden');
+  elementBody.classList.remove('modal-open');
+  uploadCancel.removeEventListener('click', closeModal);
+  document.removeEventListener('keydown', onEscDown);
+};
+
+function onEscDown(evt) {
+  if (evt.key === 'Escape') {
+    closeModal();
+  }
+}
+
+const addHandlers = () => {
+  uploadCancel.addEventListener('click', closeModal);
+  document.addEventListener('keydown', onEscDown);
+};
+
+const showModal = () => {
+  previewPhoto.classList.remove('hidden');
+  elementBody.classList.add('modal-open');
+  addHandlers();
+};
+
 
 export const renderPreview = () => {
-  previewPhoto.classList.remove('hidden');
-  elementBody.classList.remove('modal-open');
-  // console.log(fileUploadElement);
-
-
+  showModal();
 };
