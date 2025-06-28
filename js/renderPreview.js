a67f5913d76a695ef75e6b07855cf64447b4104e
Chackmoole <cebep-21@mail.ru>
2025-06-28 10:54:59 +0300

    Добавлена проверка Хэш-Тэга по ТЗ

--- js/renderPreview.js
+++ js/renderPreview.js
@@ -1,6 +1,9 @@
+import { validationHashTag } from './validationHashTag.js';
+
 const previewPhoto = document.querySelector('.img-upload__overlay');
 const elementBody = document.querySelector('body');
 const uploadCancel = document.querySelector('#upload-cancel');
+const submitButton = document.querySelector('.img-upload__form');
 
 
 const closeModal = () => {
@@ -27,7 +30,15 @@ const showModal = () => {
   addHandlers();
 };
 
+const addSubmitHandler = () => {
+  submitButton.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    validationHashTag();
+
+  });
+};
 
 export const renderPreview = () => {
   showModal();
+  addSubmitHandler();
 };
