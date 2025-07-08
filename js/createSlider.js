f7cffe80de0f8c40f664674e67cfb1cf187ef2b8
Chackmoole <cebep-21@mail.ru>
2025-07-08 10:54:22 +0300

    Доработка функционала по наложению эффектов по ТЗ

--- js/createSlider.js
+++ js/createSlider.js
@@ -18,6 +18,7 @@ const createSlider = () => {
     step: 0.1,
     connect: 'lower'
   });
+  sliderElement.setAttribute('disabled', true);
 
   sliderElement.noUiSlider.on('update', () => {
     valueElement.value = sliderElement.noUiSlider.get();
@@ -33,14 +34,22 @@ const createSlider = () => {
         uploadImage.classList.add('effects__preview--none');
         filterPhoto = 'none';
         filterUnit = '';
+        sliderElement.setAttribute('disabled', true);
+        sliderElement.noUiSlider.updateOptions({
+          range: {
+            min: 0,
+            max: 1,
+          },
+          step: 0.1,
+        });
         sliderElement.noUiSlider.set(1);
-
         break;
       case 'effect-chrome':
         uploadImage.classList.remove(...uploadImage.classList);
         uploadImage.classList.add('effects__preview--chrome');
         filterPhoto = 'grayscale';
         filterUnit = '';
+        sliderElement.removeAttribute('disabled');
         sliderElement.noUiSlider.updateOptions({
           range: {
             min: 0,
@@ -48,13 +57,14 @@ const createSlider = () => {
           },
           step: 0.1,
         });
-        sliderElement.noUiSlider.set(0.75);
+        sliderElement.noUiSlider.set(1);
         break;
       case 'effect-sepia':
         uploadImage.classList.remove(...uploadImage.classList);
         uploadImage.classList.add('effects__preview--sepia');
         filterPhoto = 'sepia';
         filterUnit = '';
+        sliderElement.removeAttribute('disabled');
         sliderElement.noUiSlider.updateOptions({
           range: {
             min: 0,
@@ -62,13 +72,14 @@ const createSlider = () => {
           },
           step: 0.1,
         });
-        sliderElement.noUiSlider.set(0.75);
+        sliderElement.noUiSlider.set(1);
         break;
       case 'effect-marvin':
         uploadImage.classList.remove(...uploadImage.classList);
         uploadImage.classList.add('effects__preview--marvin');
         filterPhoto = 'invert';
         filterUnit = '%';
+        sliderElement.removeAttribute('disabled');
         sliderElement.noUiSlider.updateOptions({
           range: {
             min: 0,
@@ -76,13 +87,14 @@ const createSlider = () => {
           },
           step: 1,
         });
-        sliderElement.noUiSlider.set(75);
+        sliderElement.noUiSlider.set(100);
         break;
       case 'effect-phobos':
         uploadImage.classList.remove(...uploadImage.classList);
         uploadImage.classList.add('effects__preview--phobos');
         filterPhoto = 'blur';
         filterUnit = 'px';
+        sliderElement.removeAttribute('disabled');
         sliderElement.noUiSlider.updateOptions({
           range: {
             min: 0,
@@ -90,13 +102,14 @@ const createSlider = () => {
           },
           step: 0.1,
         });
-        sliderElement.noUiSlider.set(2);
+        sliderElement.noUiSlider.set(3);
         break;
       case 'effect-heat':
         uploadImage.classList.remove(...uploadImage.classList);
         uploadImage.classList.add('effects__preview--heat');
         filterPhoto = 'brightness';
         filterUnit = '';
+        sliderElement.removeAttribute('disabled');
         sliderElement.noUiSlider.updateOptions({
           range: {
             min: 1,
@@ -104,7 +117,7 @@ const createSlider = () => {
           },
           step: 0.1,
         });
-        sliderElement.noUiSlider.set(2.5);
+        sliderElement.noUiSlider.set(3);
         break;
     }
   });
