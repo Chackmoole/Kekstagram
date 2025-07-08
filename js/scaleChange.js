c96b9f81a526379a18dbc7405307192b7538218f
Chackmoole <cebep-21@mail.ru>
2025-07-08 17:50:11 +0300

    Добавлено изменение масштаба загруженного фото

--- js/scaleChange.js
+++ js/scaleChange.js
@@ -2,6 +2,7 @@ const fieldScale = document.querySelector('.img-upload__scale');
 const decreaseButton = fieldScale.querySelector('.scale__control--smaller');
 const increaseButton = fieldScale.querySelector('.scale__control--bigger');
 const scaleInput = fieldScale.querySelector('.scale__control--value');
+const uploadImageElement = document.querySelector('.img-upload__preview').firstElementChild;
 
 scaleInput.value = '100%';
 
@@ -10,22 +11,23 @@ export const scaleHandler = () => {
   let scaleCount = scaleInput.value.slice(0, -1);
 
   decreaseButton.addEventListener('click', () => {
-    if (scaleCount > 0) {
+    if (scaleCount > 25) {
       decreaseButton.disabled = false;
       increaseButton.disabled = false;
       scaleCount -= 25;
-
+      uploadImageElement.style.transform = `scale(0.${scaleCount})`;
       scaleInput.value = `${scaleCount}%`;
     } else {
       decreaseButton.disabled = true;
     }
   });
   increaseButton.addEventListener('click', () => {
-    if (scaleCount < 100) {
+    if (scaleCount <= 75) {
       increaseButton.disabled = false;
       decreaseButton.disabled = false;
       scaleCount += 25;
-
+      if (scaleCount <= 75) { uploadImageElement.style.transform = `scale(0.${scaleCount})`; }
+      else { uploadImageElement.style.transform = 'scale(1)'; }
       scaleInput.value = `${scaleCount}%`;
     } else {
       increaseButton.disabled = true;
