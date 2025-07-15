0652d9e94042718485250e0fff6970f88b54d473
Chackmoole <cebep-21@mail.ru>
2025-07-15 12:38:13 +0300

    fix

--- js/api.js
+++ js/api.js
@@ -1,13 +1,8 @@
-import { renderPhotos } from './renderPhotos.js';
 import { showAlert } from './util.js';
 
-const getData = () => {
+const getData = () =>
   fetch('https://25.javascript.htmlacademy.pro/kekstagram/data')
-    .then((response) => response.json())
-    .then((data) => {
-      renderPhotos(data);
-    });
-};
+    .then((response) => response.json());
 
 const sendData = (formData) =>
   fetch('https://25.javascript.htmlacademy.pro/kekstagram',
--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 import { getData } from './api.js';
+import { renderPhotos } from './renderPhotos.js';
 
-getData();
+getData()
+  .then((data) => {
+    renderPhotos(data);
+  });
